fc7207686081cdc4e0fb43c941b277e9ff08cfb8
HsuAustin <chimimh1@uci.edu>
2025-03-08 05:48:02 +0000

    done dijkstra_shortest_path function

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -6,4 +6,29 @@
 
 using namespace std;
 
-
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+    int n = G.numVertices;
+    vector<int> dist(n, INF);
+    previous.assign(n, -1);
+    vector<bool> visited(n, false);
+    using Node = pair<int,int>; 
+    priority_queue<Node, vector<Node>, greater<Node>> pq;
+    dist[source] = 0;
+    pq.push({0, source});
+    while (!pq.empty()) {
+        auto [curDist, u] = pq.top();
+        pq.pop();
+        if (visited[u]) {continue};
+        visited[u] = true;
+        for (auto &edge : G[u]) {
+            int v = edge.dst;
+            int w = edge.weight;
+            if (!visited[v] && dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+                previous[v] = u;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+    return dist;
+}
