1a5f17483ca0645f36c18142a59cbe0a4832e85e
HsuAustin <chimimh1@uci.edu>
2025-03-08 05:51:59 +0000

    done print_path function

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -40,3 +40,16 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
     reverse(path.begin(), path.end());
     return path;
 }
+
+void print_path(const vector<int>& v, int total) {
+    if (v.empty()) {
+        cout << "No path" << endl;
+        return;
+    }
+    for (size_t i = 0; i < v.size(); i++) {
+        if (i > 0) cout << " ";
+        cout << v[i];
+    }
+    cout << endl;
+    cout << "Total cost: " << total << endl;
+}
