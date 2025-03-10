7f518fceed38e54bd08c4bf7a307e4b0685d2bb4
HsuAustin <chimimh1@uci.edu>
2025-03-10 03:45:06 +0000

    done generate_word_ladder function

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -46,3 +46,28 @@ bool edit_distance_within(const string& str1, const string& str2, int d) {
 bool is_adjacent(const string& word1, const string& word2) {
     return edit_distance_within(word1, word2, 1);
 }
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+    if (word_list.find(end_word) == word_list.end()) {return {};}
+    queue<vector<string>> ladder_queue;
+    set<string> visited;
+    ladder_queue.push({begin_word});
+    if (word_list.find(begin_word) != word_list.end()) {visited.insert(begin_word);}
+    while (!ladder_queue.empty()) {
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+        const string& last_word = ladder.back();
+        for (auto &w : word_list) {
+            if (visited.find(w) == visited.end()) {
+                if (is_adjacent(last_word, w)) {
+                    vector<string> new_ladder = ladder;
+                    new_ladder.push_back(w);
+                    if (w == end_word) {return new_ladder;}
+                    ladder_queue.push(new_ladder);
+                    visited.insert(w);
+                }
+            }
+        }
+    }
+    return {};
+}
